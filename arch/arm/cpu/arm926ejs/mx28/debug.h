fe2e5245a25e0d1773f405bc3f1545f979499f88
Lothar Waßmann <LW@KARO-electronics.de>
2012-03-14 21:53:15 +0100

    define dummy macros or extern decls depending on DEBUG

--- arch/arm/cpu/arm926ejs/mx28/debug.h
+++ arch/arm/cpu/arm926ejs/mx28/debug.h
@@ -1,3 +1,9 @@
+#ifdef DEBUG
 extern void printhex(int data);
 extern void printdec(int data);
 extern void dprintf(const char *fmt, ...);
+#else
+#define printhex(d) do { } while (0)
+#define printdec(d) do { } while (0)
+#define dprintf(fmt...) do { } while (0)
+#endif
