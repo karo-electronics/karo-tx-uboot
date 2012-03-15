3dcda7a752efb6d67471ae5dfbe4d0f30aa4f0aa
Lothar Waßmann <LW@KARO-electronics.de>
2012-03-15 14:29:25 +0100

    define dbg() macro also in non-DEBUG case

--- arch/arm/cpu/arm926ejs/mx28/debug.h
+++ arch/arm/cpu/arm926ejs/mx28/debug.h
@@ -4,10 +4,11 @@ extern void printdec(int data);
 extern void dprintf(const char *fmt, ...);
 static int debug = 1;
 #define dbg_lvl(lvl)		(debug > (lvl))
-#define dbg(lvl, fmt...)	do { if (dbg_lvl(lvl)) dprintf(fmt); } while (0)
 #else
 #define dbg_lvl(lvl)		0
 #define printhex(d)		do { } while (0)
 #define printdec(d)		do { } while (0)
 #define dprintf(lvl, fmt...)	do { } while (0)
 #endif
+
+#define dbg(lvl, fmt...)	do { if (dbg_lvl(lvl)) dprintf(fmt); } while (0)
