14c8d7cc6b154f45536b7bffcc3f690c0364fed9
Lothar Waßmann <LW@KARO-electronics.de>
2013-06-24 11:04:00 +0200

    clear the reset status flags in reset_cpu()

--- arch/arm/cpu/armv7/omap-common/reset.c
+++ arch/arm/cpu/armv7/omap-common/reset.c
@@ -25,6 +25,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
  * MA 02111-1307 USA
  */
+#include <common.h>
 #include <config.h>
 #include <asm/io.h>
 #include <asm/arch/cpu.h>
@@ -32,6 +33,8 @@
 
 void __weak reset_cpu(unsigned long ignored)
 {
+	/* clear the reset status flags */
+	writel(readl(PRM_RSTST), PRM_RSTST);
 	writel(PRM_RSTCTRL_RESET, PRM_RSTCTRL);
 }
 
