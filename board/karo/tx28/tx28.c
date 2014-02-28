81086b70d447344d44b579adfb25aa52cdf1ac87
Lothar Waßmann <LW@KARO-electronics.de>
2014-02-28 12:21:28 +0100

    karo: tx28: resize fdt in ft_board_setup()

--- board/karo/tx28/tx28.c
+++ board/karo/tx28/tx28.c
@@ -966,6 +966,11 @@ void ft_board_setup(void *blob, bd_t *bd)
 	const char *baseboard = getenv("baseboard");
 	int stk5_v5 = baseboard != NULL && (strcmp(baseboard, "stk5-v5") == 0);
 	const char *video_mode = karo_get_vmode(getenv("video_mode"));
+	int ret;
+
+	ret = fdt_increase_size(blob, 4096);
+	if (ret)
+		printf("Failed to increase FDT size: %s\n", fdt_strerror(ret));
 
 #ifdef CONFIG_TX28_S
 	/* TX28-41xx (aka TX28S) has no external RTC
