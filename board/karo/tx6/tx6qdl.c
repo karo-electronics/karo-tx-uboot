68cced36557286995ae54516e723c51959aa7901
Lothar Waßmann <LW@KARO-electronics.de>
2015-03-03 13:14:02 +0100

    karo: tx6: improve pad ctrl for SD card interfaces

--- board/karo/tx6/tx6qdl.c
+++ board/karo/tx6/tx6qdl.c
@@ -301,8 +301,8 @@ void dram_init_banksize(void)
 
 #ifdef	CONFIG_CMD_MMC
 #define USDHC_PAD_CTRL (PAD_CTL_PUS_47K_UP |			\
-	PAD_CTL_SPEED_LOW | PAD_CTL_DSE_80ohm |			\
-	PAD_CTL_SRE_FAST  | PAD_CTL_HYS)
+	PAD_CTL_SPEED_MED | PAD_CTL_DSE_40ohm |		\
+	PAD_CTL_SRE_FAST)
 
 static const iomux_v3_cfg_t mmc0_pads[] = {
 	MX6_PAD_SD1_CMD__USDHC1_CMD | MUX_PAD_CTRL(USDHC_PAD_CTRL),
