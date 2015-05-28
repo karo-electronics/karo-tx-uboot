1af6b3d58c448268150e73c5ac3215e448d9221b
Fabio Estevam <fabio.estevam at freescale.com>
2015-05-28 12:33:34 -0300

    mx6_common: Fix LOADADDR and SYS_TEXT_BASE for MX6SL and MX6SX
    Commit 8183058188cd2d942 ("imx6: centralise common boot options in
    mx6_common.h") broke boot on mx6sl and mx6sx by assuming that all mx6
    SoCs use the same LOADADDR/SYS_TEXT_BASE range, which is not correct.
    
    DDR on mx6sx/mx6sl starts at 0x80000000.
    
    Adjust LOADADDR/SYS_TEXT_BASE to the proper values for mx6sx/mx6sl,
    so that these SoCs can boot again.
    
    Also, TQMA6 requires a custom CONFIG_SYS_TEXT_BASE value, so move
    its setting prior to the inclusion of mx6_common.h.
    
    Signed-off-by: Fabio Estevam <fabio.estevam at freescale.com>

--- include/configs/tqma6.h
+++ include/configs/tqma6.h
@@ -22,6 +22,13 @@
 
 /* #endif */
 
+/* place code in last 4 MiB of RAM */
+#if defined(CONFIG_MX6DL) || defined(CONFIG_MX6S)
+#define CONFIG_SYS_TEXT_BASE		0x2fc00000
+#elif defined(CONFIG_MX6Q) || defined(CONFIG_MX6D)
+#define CONFIG_SYS_TEXT_BASE		0x4fc00000
+#endif
+
 #include "mx6_common.h"
 
 #define PHYS_SDRAM_SIZE			(512u * SZ_1M)
@@ -113,11 +120,6 @@
 /* Command definition */
 #define CONFIG_CMD_BMODE
 #define CONFIG_CMD_ITEST
-
-/* place code in last 4 MiB of RAM */
-#define CONFIG_SYS_TEXT_BASE		0x2fc00000
-#define CONFIG_SYS_TEXT_BASE		0x4fc00000
-#endif
 
 #define CONFIG_ENV_SIZE			(SZ_8K)
 /* Size of malloc() pool */
