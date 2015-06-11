2819e0a8ba7e24f317caf3d829657ddd131bf807
Peng Fan <Peng.Fan@freescale.com>
2015-06-11 18:30:37 +0800

    imx: mx6 introuduce macro is_mx6dqp
    Add a new revision CHIP_REV_2_0.
    Introudce macro is_mx6dqp, dqp means Dual/Quad Plus.
    Since Dual/Quad Plus use same cpu type with Dual/Quad, but different
    revision(Major Lower), we use this macro for Dual/Quad Plus.
    
    Signed-off-by: Ye.Li <B37916@freescale.com>
    Signed-off-by: Peng Fan <Peng.Fan@freescale.com>

--- arch/arm/include/asm/arch-mx6/sys_proto.h
+++ arch/arm/include/asm/arch-mx6/sys_proto.h
@@ -30,6 +30,9 @@ const char *get_imx_type(u32 imxtype);
 unsigned imx_ddr_size(void);
 void set_chipselect_size(int const);
 
+#define is_mx6dqp() ((is_cpu_type(MXC_CPU_MX6Q) || \
+		     is_cpu_type(MXC_CPU_MX6D)) && \
+		     (soc_rev() >= CHIP_REV_2_0))
 
 struct mxs_register_32;
 
