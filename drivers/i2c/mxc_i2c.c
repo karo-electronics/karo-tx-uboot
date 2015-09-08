TobleMiner/u-boot-cmm2

arm, imx6, i2c: add I2C4 for MX6DL
add I2C4 modul for MX6DL based boards.

Signed-off-by: Heiko Schocher <hs@denx.de>

drivers/i2c/mxc_i2c.c
@@ -517,6 +517,9 @@ static struct mxc_i2c_bus mxc_i2c_buses[] = {
 	{ 0, I2C1_BASE_ADDR },
 	{ 1, I2C2_BASE_ADDR },
 	{ 2, I2C3_BASE_ADDR },
+#if defined(CONFIG_MX6DL)
+	{ 3, I2C4_BASE_ADDR },
+#endif
 #elif defined(CONFIG_LS102XA)
 	{ 0, I2C1_BASE_ADDR, I2C_QUIRK_FLAG },
 	{ 1, I2C2_BASE_ADDR, I2C_QUIRK_FLAG },

karo-electronics/karo-tx-uboot

drivers/i2c/mxc_i2c.c
@@ -526,6 +526,9 @@ static struct mxc_i2c_bus mxc_i2c_buses[] = {
 	{ 0, I2C1_BASE_ADDR },
 	{ 1, I2C2_BASE_ADDR },
 	{ 2, I2C3_BASE_ADDR },
+#if defined(CONFIG_SOC_MX6DL)
+	{ 3, I2C4_BASE_ADDR },
+#endif
 #elif defined(CONFIG_SOC_LS102XA)
 	{ 0, I2C1_BASE_ADDR, I2C_QUIRK_FLAG },
 	{ 1, I2C2_BASE_ADDR, I2C_QUIRK_FLAG },