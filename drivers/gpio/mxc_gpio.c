TobleMiner/u-boot-cmm2

mxc: gpio add i.MX6UL support
i.MX6UL does not have GPIO6/7, so do not include them for i.MX6UL.

Signed-off-by: Peng Fan <Peng.Fan@freescale.com>

drivers/gpio/mxc_gpio.c
@@ -45,10 +45,14 @@ static unsigned long gpio_ports[] = {
 #endif
 #if defined(CONFIG_MX27) || defined(CONFIG_MX53) || defined(CONFIG_MX6)
 	[4] = GPIO5_BASE_ADDR,
+#ifndef CONFIG_MX6UL
 	[5] = GPIO6_BASE_ADDR,
 #endif
+#endif
 #if defined(CONFIG_MX53) || defined(CONFIG_MX6)
+#ifndef CONFIG_MX6UL
 	[6] = GPIO7_BASE_ADDR,
+#endif
 #endif
 };
 

karo-electronics/karo-tx-uboot

drivers/gpio/mxc_gpio.c
@@ -45,10 +45,14 @@ static unsigned long gpio_ports[] = {
 #endif
 #if defined(CONFIG_SOC_MX27) || defined(CONFIG_SOC_MX53) || defined(CONFIG_SOC_MX6)
 	[4] = GPIO5_BASE_ADDR,
+#ifndef CONFIG_SOX_MX6UL
 	[5] = GPIO6_BASE_ADDR,
 #endif
+#endif
 #if defined(CONFIG_SOC_MX53) || defined(CONFIG_SOC_MX6)
+#ifndef CONFIG_SOC_MX6UL
 	[6] = GPIO7_BASE_ADDR,
+#endif
 #endif
 };
 