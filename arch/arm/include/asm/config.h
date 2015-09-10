TobleMiner/u-boot-cmm2

arm: pxa: use common timer functions
This patch moves pxa to the common timer functions added in commit

  8dfafdd - Introduce common timer functions <Rob Herring>

The (removed) pxa timer code (specifically __udelay()) could deadlock at
the 32-bit boundary of get_ticks().  get_ticks() returned a 32-bit value
cast up to a 64-bit value.  If get_ticks() + tmo in __udelay() crossed
the 32-bit boundary, the while condition became unconditionally true and
locked the processor.  Rather than patch the specific pxa issues, simply
move everything over to the common code.

Signed-off-by: Andrew Ruder <andrew.ruder@elecsyscorp.com>
Cc: Marek Vasut <marex@denx.de>
Cc: Tom Rini <trini@konsuiko.com>

arch/arm/include/asm/config.h
@@ -19,7 +19,10 @@
 #include <asm/arch-fsl-lsch3/config.h>
 #endif
 
-#ifdef CONFIG_LS102XA
+#if defined(CONFIG_LS102XA) || \
+	defined(CONFIG_CPU_PXA27X) || \
+	defined(CONFIG_CPU_MONAHANS) || \
+	defined(CONFIG_CPU_PXA25X)
 #include <asm/arch/config.h>
 #endif
 

karo-electronics/karo-tx-uboot

arch/arm/include/asm/config.h
@@ -19,7 +19,10 @@
 #include <asm/arch-fsl-lsch3/config.h>
 #endif
 
-#ifdef CONFIG_SOC_LS102XA
+#if defined(CONFIG_SOC_LS102XA) || \
+	defined(CONFIG_CPU_PXA27X) || \
+	defined(CONFIG_CPU_MONAHANS) || \
+	defined(CONFIG_CPU_PXA25X)
 #include <asm/arch/config.h>
 #endif
 