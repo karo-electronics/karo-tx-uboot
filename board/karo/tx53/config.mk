6eb9c03b43586d536df11dba254e30d265f93a63
Lothar Waßmann <LW@KARO-electronics.de>
2016-06-30 10:44:39 +0200

    karo: tx53: various fixes for CONFIG_SECURE_BOOT

--- board/karo/tx53/config.mk
+++ board/karo/tx53/config.mk
@@ -2,6 +2,12 @@
 CONFIG_SYS_TEXT_BASE := 0x700ff000
 
 __HAVE_ARCH_GENERIC_BOARD := y
+
 PLATFORM_CPPFLAGS += -Werror
+ifneq ($(CONFIG_SECURE_BOOT),)
+#	promote config variable to linker script
+	PLATFORM_CPPFLAGS += -DCONFIG_SECURE_BOOT
+endif
+
 LOGO_BMP = logos/karo.bmp
 OBJCOPYFLAGS += -j .pad
