karo-electronics/karo-tx-uboot

commit 9b2cc7dbfd46b61f9f44cbfe3eb10d5478e152d1

imx: mx6 add PAD_CTL_SPEED_LOW for i.MX6SX/UL
PAD_CTL_SPEED_LOW for i.MX6SX/UL is (0 << 6)

Signed-off-by: Ye.Li <B37916@freescale.com>
Signed-off-by: Peng Fan <Peng.Fan@freescale.com>

arch/arm/include/asm/imx-common/iomux-v3.h
@@ -105,8 +105,8 @@ typedef u64 iomux_v3_cfg_t;
 
 #define PAD_CTL_ODE		__MUX_PAD_CTRL(1 << 11)
 
-#define PAD_CTL_SPEED_LOW	__MUX_PAD_CTRL(1 << 6)
-#define PAD_CTL_SPEED_MED	__MUX_PAD_CTRL(2 << 6)
+#define PAD_CTL_SPEED_LOW	__MUX_PAD_CTRL(0 << 6)
+#define PAD_CTL_SPEED_MED	__MUX_PAD_CTRL(1 << 6)
 #define PAD_CTL_SPEED_HIGH	__MUX_PAD_CTRL(3 << 6)
 
 #define PAD_CTL_DSE_DISABLE	__MUX_PAD_CTRL(0 << 3)

commit 66b6c2cf7d3ca64d3cf7b3c16b36d0814bb84a8f

imx: mx6: align the SPEED_MED setting with the reset default
The PAD_CTL SPEED settings 0x1 and 0x2 both designate 'Medium
Speed'. The PAD_CTL register HW reset default value is '0x2' for
most pads, so use this value for the PAD_CTL_SPEED_MED definition.

This partially reverts commit 9b2cc7dbfd46 ("imx: mx6 add
PAD_CTL_SPEED_LOW for i.MX6SX/UL") which inadvertently changed the
definition from '2' to '1'.

arch/arm/include/asm/imx-common/iomux-v3.h
@@ -106,7 +106,7 @@ typedef u64 iomux_v3_cfg_t;
 #define PAD_CTL_ODE		__MUX_PAD_CTRL(1 << 11)
 
 #define PAD_CTL_SPEED_LOW	__MUX_PAD_CTRL(0 << 6)
-#define PAD_CTL_SPEED_MED	__MUX_PAD_CTRL(1 << 6)
+#define PAD_CTL_SPEED_MED	__MUX_PAD_CTRL(2 << 6)
 #define PAD_CTL_SPEED_HIGH	__MUX_PAD_CTRL(3 << 6)
 
 #define PAD_CTL_DSE_DISABLE	__MUX_PAD_CTRL(0 << 3)