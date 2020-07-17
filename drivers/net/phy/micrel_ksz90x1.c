6ae574d1cda5eab713afa21c957f029e03fe706a
Lothar Waßmann <LW@KARO-electronics.de>
2020-07-17 10:23:29 +0200

    ksz90x1: force master mode for 1000BaseT due to chip errata

--- drivers/net/phy/micrel_ksz90x1.c
+++ drivers/net/phy/micrel_ksz90x1.c
@@ -356,6 +356,7 @@ static int ksz9031_config(struct phy_device *phydev)
 {
 	int ret;
 	u16 bmcr;
+	unsigned int features = phydev->drv->features;
 
 	/*
 	 * Check if Power-Down bit from Basic Control Register is set.
@@ -377,11 +378,9 @@ static int ksz9031_config(struct phy_device *phydev)
 
 	/* add an option to disable the gigabit feature of this PHY */
 	if (env_get("disable_giga")) {
-		unsigned features;
 		unsigned bmcr;
 
 		/* disable speed 1000 in features supported by the PHY */
-		features = phydev->drv->features;
 		features &= ~(SUPPORTED_1000baseT_Half |
 				SUPPORTED_1000baseT_Full);
 		phydev->advertising = phydev->supported = features;
@@ -399,6 +398,18 @@ static int ksz9031_config(struct phy_device *phydev)
 		genphy_restart_aneg(phydev);
 
 		return 0;
+	} else {
+		/* force master mode for 1000BaseT due to chip errata */
+		uint16_t ctrl1000 = 0;
+		const uint16_t master = CTRL1000_PREFER_MASTER |
+			CTRL1000_CONFIG_MASTER | CTRL1000_MANUAL_CONFIG;
+
+		if (features & SUPPORTED_1000baseT_Half)
+			ctrl1000 |= ADVERTISE_1000HALF | master;
+		if (features & SUPPORTED_1000baseT_Full)
+			ctrl1000 |= ADVERTISE_1000FULL | master;
+
+		phy_write(phydev, MDIO_DEVAD_NONE, MII_CTRL1000, ctrl1000);
 	}
 
 	return genphy_config(phydev);
