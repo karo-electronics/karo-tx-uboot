2276629a25137c1fb92afd43ebdd60d49695f1d3
Lothar Waßmann <LW@KARO-electronics.de>
2020-10-26 09:35:40 +0100

    arm: stm32mp: fix compile error when MMC_SUPPORT is not enabled

--- arch/arm/mach-stm32mp/spl.c
+++ arch/arm/mach-stm32mp/spl.c
@@ -50,6 +50,7 @@ u32 spl_boot_device(void)
 	return BOOT_DEVICE_MMC1;
 }
 
+#if CONFIG_IS_ENABLED(MMC_SUPPORT)
 u32 spl_mmc_boot_mode(const u32 boot_device)
 {
 	return MMCSD_MODE_RAW;
@@ -66,6 +67,7 @@ int spl_mmc_boot_partition(const u32 boot_device)
 		return -EINVAL;
 	}
 }
+#endif
 
 #ifdef CONFIG_SPL_DISPLAY_PRINT
 void spl_display_print(void)
