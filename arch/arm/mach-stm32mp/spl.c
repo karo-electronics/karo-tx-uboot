b6319109cc73e1a3a0f4fe045a5450996179dafc
Lothar Waßmann <LW@KARO-electronics.de>
2020-11-03 14:17:10 +0100

    arm: stm32mp: add support for booting from spi-nand

--- arch/arm/mach-stm32mp/spl.c
+++ arch/arm/mach-stm32mp/spl.c
@@ -44,7 +44,7 @@ u32 spl_boot_device(void)
 	case BOOT_FLASH_NOR_QSPI:
 		return BOOT_DEVICE_SPI;
 	case BOOT_FLASH_SPINAND_1:
-		return BOOT_DEVICE_NONE; /* SPINAND not supported in SPL */
+		return BOOT_DEVICE_MTD;
 	}
 
 	return BOOT_DEVICE_MMC1;
