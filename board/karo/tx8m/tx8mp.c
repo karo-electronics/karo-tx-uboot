98f51b14ed0b9fefdd3fa9b1febfefce4757b414
Lothar Waßmann <LW@KARO-electronics.de>
2021-01-20 17:33:39 +0100

    karo: tx8m: add support for TX8P module

--- board/karo/tx8m/tx8mp.c
+++ board/karo/tx8m/tx8mp.c
@@ -6,6 +6,7 @@
 
 #include <common.h>
 #include <console.h>
+#include <debug_uart.h>
 #include <dwc3-uboot.h>
 #include <errno.h>
 #include <fdt_support.h>
@@ -42,27 +43,74 @@
 
 DECLARE_GLOBAL_DATA_PTR;
 
-enum tx8m_boardtype {
-	QSXP,
-	QSBASE3,
-	NUM_BOARD_TYPES
-};
-
 #if IS_ENABLED(CONFIG_NET)
-static int qsxp_setup_eth(enum tx8m_boardtype board)
+#define ENET_QOS_CLK_TX_CLK_SEL		BIT(20)
+#define ENET_QOS_RGMII_EN		BIT(21)
+#define ENET_QOS_CLK_GEN_EN		BIT(19)
+#define ENET_QOS_INTF_SEL_RMII		(4 * BIT(16))
+#define ENET_QOS_INTF_SEL_RGMII		(1 * BIT(16))
+
+static int setup_eqos(void)
 {
+	int ret;
 	struct iomuxc_gpr_base_regs *iomuxc_gpr_regs =
 		(void *)IOMUXC_GPR_BASE_ADDR;
 	unsigned char mac[6];
+	const char *phy_mode;
+	u32 gpr1_val = ENET_QOS_CLK_GEN_EN;
+	ofnode eth_node = ofnode_path("ethernet0");
 
-	if (board == QSXP) {
-		/* Use 50M anatop REF_CLK1 for ENET, not from external */
-		setbits_le32(&iomuxc_gpr_regs->gpr[1],
-			     IOMUXC_GPR_GPR1_GPR_ENET1_TX_CLK_SEL_MASK);
+	if (!ofnode_is_available(eth_node)) {
+		printf("No ethernet support\n");
+		return ret;
+	}
+	debug("eth_node=%s\n", ofnode_get_name(eth_node));
+	phy_mode = ofnode_read_string(eth_node, "phy-mode");
+	if (!phy_mode) {
+		printf("phy-mode property not found in %s\n",
+		       ofnode_get_name(eth_node));
+		return -ENODEV;
+	}
+
+	if (strcmp(phy_mode, "rmii") == 0) {
+		struct udevice *reg_3v3_etn;
+		const char *reg_name = "3v3-etn";
+
+		debug("Setting up EQOS in RMII mode\n");
+		clrsetbits_le32(&iomuxc_gpr_regs->gpr[1],
+				IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK |
+				ENET_QOS_RGMII_EN,
+				ENET_QOS_CLK_TX_CLK_SEL |
+				gpr1_val | ENET_QOS_INTF_SEL_RMII);
+		ret = regulator_get_by_platname(reg_name,
+						&reg_3v3_etn);
+		if (ret) {
+			printf("Failed to get %s regulator: %d\n",
+			       reg_name, ret);
+		} else {
+			debug("%s@%d: Enabling %s regulator\n",
+			      __func__, __LINE__, reg_name);
+			ret = regulator_set_enable(reg_3v3_etn, 1);
+			if (ret)
+				printf("Failed to enable %s regulator: %d\n",
+				       reg_name, ret);
+		}
+		ret = set_clk_eqos(ENET_50MHZ);
 	} else {
-		/* Use 125M anatop REF_CLK1 for ENET, not from external */
-		setbits_le32(&iomuxc_gpr_regs->gpr[1],
-			     IOMUXC_GPR_GPR1_GPR_ENET1_TX_CLK_SEL_MASK);
+		debug("Setting up EQOS in RGMII mode\n");
+		/* set INTF as RGMII, enable RGMII TXC clock */
+		clrsetbits_le32(&iomuxc_gpr_regs->gpr[1],
+				IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK,
+				gpr1_val | ENET_QOS_INTF_SEL_RGMII |
+				ENET_QOS_RGMII_EN);
+
+		ret = set_clk_eqos(ENET_125MHZ);
+	}
+	debug("%s@%d: GPR1=%08x\n", __func__, __LINE__,
+	       readl(&iomuxc_gpr_regs->gpr[1]));
+	if (ret) {
+		printf("Failed to set EQOS refclk: %d\n", ret);
+		return ret;
 	}
 
 	if (!env_get("ethaddr")) {
@@ -74,43 +122,18 @@ static int qsxp_setup_eth(enum tx8m_boardtype board)
 	return 0;
 }
 #else
-static inline int qsxp_setup_eth(enum tx8m_boardtype board)
-{
-	return 0;
-}
-#endif /* CONFIG_NET */
-
-#if IS_ENABLED(CONFIG_DWC_ETH_QOS)
-static int setup_eqos(void)
-{
-	struct iomuxc_gpr_base_regs *gpr =
-		(struct iomuxc_gpr_base_regs *)IOMUXC_GPR_BASE_ADDR;
-
-	/* set INTF as RGMII, enable RGMII TXC clock */
-	clrsetbits_le32(&gpr->gpr[1],
-			IOMUXC_GPR_GPR1_GPR_ENET_QOS_INTF_SEL_MASK, BIT(16));
-	setbits_le32(&gpr->gpr[1], BIT(19) | BIT(21));
-
-	return set_clk_eqos(ENET_125MHZ);
-}
-#else
 static inline int setup_eqos(void)
 {
 	return 0;
 }
-#endif
-
-int board_fix_fdt(void *blob)
-{
-	return 0;
-}
+#endif /* CONFIG_NET */
 
 static struct udevice *thermaldev __section(.data);
 
 #define TEMPERATURE_HOT		80
 #define TEMPERATURE_MIN		-40
 
-static void qsxp_show_temp(void)
+static void board_show_temp(void)
 {
 	int ret;
 	int cpu_temp;
@@ -163,16 +186,36 @@ static void qsxp_show_temp(void)
 #ifdef CONFIG_DISPLAY_BOARDINFO
 int checkboard(void)
 {
+	if (IS_ENABLED(CONFIG_DEBUG_UART)) {
+		debug_uart_init();
+		printascii("enabled\n");
+	}
+
+#if defined(CONFIG_KARO_TX8P_ML81)
+	printf("Board: Ka-Ro TX8P-ML81\n");
+#elif defined(CONFIG_KARO_QSXP_ML81)
 	printf("Board: Ka-Ro QSXP-ML81\n");
+#else
+#error Unsupported module variant
+#endif /* CONFIG_KARO_TX8P_ML81 */
 
-	ctrlc();
+	board_show_temp();
+	return 0;
+}
+#endif /* CONFIG_DISPLAY_BOARDINFO */
 
-	qsxp_show_temp();
+#ifdef CONFIG_OF_BOARD_FIXUP
+int board_fix_fdt(void *blob)
+{
 	return 0;
 }
 #endif
 
 #ifdef CONFIG_USB_DWC3
+#define USB_CTL1_ADDR			0xF0004
+#define USB_CTL1_OCPOL			BIT(16)
+#define USB_CTL1_PWRPOL			BIT(17)
+
 #define USB_PHY_CTRL0			0xF0040
 #define USB_PHY_CTRL0_REF_SSP_EN	BIT(2)
 
@@ -193,19 +236,30 @@ int checkboard(void)
 #define HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN_SHIFT    1
 #define HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN	    (0x1U << HSIO_GPR_REG_0_USB_CLOCK_MODULE_EN_SHIFT)
 
-static struct dwc3_device dwc3_device_data = {
-	.maximum_speed = USB_SPEED_SUPER,
-	.base = USB1_BASE_ADDR,
-	.dr_mode = USB_DR_MODE_PERIPHERAL,
-	.index = 0,
-	.power_down_scale = 2,
+static struct dwc3_device dwc3_device_data[] = {
+	{
+		.maximum_speed = USB_SPEED_SUPER,
+		.base = USB1_BASE_ADDR,
+		.dr_mode = USB_DR_MODE_PERIPHERAL,
+		.index = 0,
+		.power_down_scale = 2,
+	},
+	{
+		.maximum_speed = USB_SPEED_SUPER,
+		.base = USB2_BASE_ADDR,
+		.dr_mode = USB_DR_MODE_PERIPHERAL,
+		.index = 1,
+		.power_down_scale = 2,
+	},
 };
 
+#if !CONFIG_IS_ENABLED(DM_USB_GADGET)
 int usb_gadget_handle_interrupts(void)
 {
 	dwc3_uboot_handle_interrupt(0);
 	return 0;
 }
+#endif
 
 static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
 {
@@ -218,7 +272,7 @@ static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
 
 	/* USB3.0 PHY signal fsel for 100M ref */
 	RegData = readl(dwc3->base + USB_PHY_CTRL0);
-	RegData = (RegData & 0xfffff81f) | (0x2a << 5);
+	RegData = (RegData & ~(0x3f << 5)) | (0x2a << 5);
 	writel(RegData, dwc3->base + USB_PHY_CTRL0);
 
 	RegData = readl(dwc3->base + USB_PHY_CTRL6);
@@ -247,55 +301,87 @@ static void dwc3_nxp_usb_phy_init(struct dwc3_device *dwc3)
 
 #if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
 
-static struct udevice *reg_vbus;
-static struct udevice *usbdev;
+static struct udevice *reg_vbus[2];
+static struct udevice *usbdev[2];
 
-static int board_enable_vbus(int index, bool enable)
+static int tx8mp_enable_vbus(unsigned int index, bool enable)
 {
 	int ret;
 
-	if (reg_vbus)
-		return 0;
+	if (index >= ARRAY_SIZE(reg_vbus))
+		return -EINVAL;
 
-	debug("Looking for USBH VBUS regulator\n");
-	ret = uclass_get_device(UCLASS_USB, index, &usbdev);
-	if (ret) {
-		printf("Failed to get USB device: %d\n", ret);
-		return ret;
+	if (!reg_vbus[index]) {
+		debug("Looking for USB%d VBUS regulator\n", index);
+		ret = uclass_get_device(UCLASS_USB, index, &usbdev[index]);
+		if (ret) {
+			printf("Failed to get usb%u device: %d\n",
+			       index, ret);
+			return ret;
+		}
+		ret = uclass_get_device_by_phandle(UCLASS_REGULATOR,
+						   usbdev[index],
+						   "vbus-supply",
+						   &reg_vbus[index]);
+		if (ret) {
+			dev_err(usbdev[index], "Failed to find VBUS regulator: %d\n",
+				ret);
+			return ret;
+		}
 	}
-	ret = uclass_get_device_by_phandle(UCLASS_REGULATOR,
-					   usbdev,
-					   "vbus-supply",
-					   &reg_vbus);
-	if (ret) {
-		dev_err(usbdev, "Failed to find VBUS regulator: %d\n",
-			ret);
-		return ret;
-	}
-
-	dev_dbg(reg_vbus, "%sabling USBH VBUS regulator\n",
-		enable ? "En" : "Dis");
-	ret = regulator_set_enable(reg_vbus, enable);
+	dev_dbg(reg_vbus[index], "%sabling USB%d VBUS regulator\n",
+		enable ? "En" : "Dis", index);
+	ret = regulator_set_enable(reg_vbus[index], enable);
 	if (ret)
-		dev_err(reg_vbus, "Failed to %sable VBUS regulator: %d\n",
+		dev_err(reg_vbus[index], "Failed to %sable VBUS regulator: %d\n",
 			enable ? "en" : "dis", ret);
 	return ret;
 }
 
-int board_usb_init(int index, enum usb_init_type init)
+static inline void tx8mp_set_usb_ctl(struct udevice *dev)
 {
-	imx8m_usb_power(index, true);
+	fdt_addr_t addr;
+	bool ocpol, pwrpol;
 
-	if (index == 0 && init == USB_INIT_DEVICE) {
-		dwc3_nxp_usb_phy_init(&dwc3_device_data);
-		return dwc3_uboot_init(&dwc3_device_data);
-	} else if (index == 0 && init == USB_INIT_HOST) {
-		return 0;
-	} else if (index == 1 && init == USB_INIT_HOST) {
-		board_enable_vbus(index, true);
+	addr = dev_read_addr(dev);
+	if (IS_ERR_VALUE(addr)) {
+		dev_err(dev, "Failed to get address: %lld\n", addr);
+		return;
 	}
 
-	return 0;
+	ocpol = dev_read_bool(dev, "oc-active-low");
+	pwrpol = dev_read_bool(dev, "pwr-active-low");
+	dev_dbg(dev, "OC is active %s\n", ocpol ? "LOW" : "HIGH");
+	dev_dbg(dev, "PWR is active %s\n", pwrpol ? "LOW" : "HIGH");
+
+	debug("USB[%08llx]: ocpol: %d pwrpol: %d\n", addr, ocpol, pwrpol);
+	debug("USB_CTL1[%08llx] changed from %08x", addr + USB_CTL1_ADDR,
+	      readl(addr + USB_CTL1_ADDR));
+	clrsetbits_le32(addr + USB_CTL1_ADDR,
+			USB_CTL1_OCPOL | USB_CTL1_PWRPOL,
+			(ocpol * USB_CTL1_OCPOL) |
+			(pwrpol * USB_CTL1_PWRPOL));
+	debug(" to %08x\n", readl(addr + USB_CTL1_ADDR));
+}
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	int ret = -EINVAL;
+
+	debug("%s(%d, %d)\n", __func__, index, init);
+
+	imx8m_usb_power(index, true);
+
+	if (init == USB_INIT_DEVICE) {
+		dwc3_nxp_usb_phy_init(&dwc3_device_data[index]);
+		ret = dwc3_uboot_init(&dwc3_device_data[index]);
+	} else if (init == USB_INIT_HOST) {
+		ret = tx8mp_enable_vbus(index, true);
+		tx8mp_set_usb_ctl(usbdev[index]);
+	}
+	if (ret)
+		imx8m_usb_power(index, false);
+	return ret;
 }
 
 int board_usb_cleanup(int index, enum usb_init_type init)
@@ -303,7 +389,7 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 	if (index == 0 && init == USB_INIT_DEVICE)
 		dwc3_uboot_exit(index);
 	else if (index == 1 && init == USB_INIT_HOST)
-		board_enable_vbus(index, false);
+		tx8mp_enable_vbus(index, false);
 
 	imx8m_usb_power(index, false);
 
@@ -318,6 +404,9 @@ int board_usb_cleanup(int index, enum usb_init_type init)
 
 int board_init(void)
 {
+	if (ctrlc())
+		printf("<CTRL-C> detected; safeboot enabled\n");
+
 #if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
 	init_usb_clk();
 #endif
@@ -331,8 +420,6 @@ int board_init(void)
 #ifdef CONFIG_BOARD_EARLY_INIT_R
 int board_early_init_r(void)
 {
-	if (ctrlc())
-		printf("<CTRL-C> detected; safeboot enabled\n");
 	return 0;
 }
 #endif
@@ -344,14 +431,10 @@ int board_late_init(void)
 	struct watchdog_regs *wdog = (void *)WDOG1_BASE_ADDR;
 	u32 srsr = readl(&src_regs->srsr);
 	u16 wrsr = readw(&wdog->wrsr);
-	const char *fdt_file = env_get("fdt_file");
-	enum tx8m_boardtype board = QSXP;
-	const char *baseboard = env_get("baseboard");
-
-	if (baseboard && strcmp(baseboard, "qsbase3") == 0)
-		board = QSBASE3;
+	const char *fdt_file;
 
 	karo_env_cleanup();
+
 	if (srsr & 0x10 && !(wrsr & WRSR_SFTW)) {
 		printf("Watchog reset detected; reboot required!\n");
 		env_set("wdreset", "1");
@@ -359,6 +442,8 @@ int board_late_init(void)
 	if (had_ctrlc()) {
 		env_set("safeboot", "1");
 		fdt_file = NULL;
+	} else {
+		fdt_file = env_get("fdt_file");
 	}
 	if (fdt_file) {
 		ret = karo_load_fdt(fdt_file);
@@ -368,7 +453,6 @@ int board_late_init(void)
 	}
 
 	setup_eqos();
-	qsxp_setup_eth(board);
 
 	clear_ctrlc();
 	return 0;
