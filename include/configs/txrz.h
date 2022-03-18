d949e1b57220d87ce5c788c8d5b9092683bf0022
Markus Bauer <mb@karo-electronics.de>
2022-03-18 14:51:49 +0100

    env: move default env to file

--- include/configs/txrz.h
+++ include/configs/txrz.h
@@ -58,19 +58,6 @@
 /* The HF/QSPI layout permits up to 1 MiB large bootloader blob */
 #define CONFIG_BOARD_SIZE_LIMIT		1048576
 
-/* ENV setting */
-
-#define CONFIG_EXTRA_ENV_SETTINGS \
-	"bootm_size=0x10000000 \0" \
-	"prodsdbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk1p1 \0" \
-	"prodemmcbootargs=setenv bootargs rw rootwait earlycon root=/dev/mmcblk0p1 \0" \
-	"bootimage=unzip 0x4A080000 0x48080000; booti 0x48080000 - 0x48000000 \0" \
-	"emmcload=ext4load mmc 0:1 0x4A080000 boot/Image.gz;ext4load mmc 0:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodemmcbootargs \0" \
-	"sd1load=ext4load mmc 1:1 0x4A080000 boot/Image.gz;ext4load mmc 1:1 0x48000000 boot/r9a07g044l-smarc-rzg2l.dtb;run prodsdbootargs \0" \
-	"bootcmd_check=if mmc dev 1; then run sd1load; else run emmcload; fi \0"
-
-#define CONFIG_BOOTCOMMAND	"run bootcmd_check;run bootimage"
-
 /* For board */
 /* Ethernet RAVB */
 #define CONFIG_BITBANGMII_MULTI
