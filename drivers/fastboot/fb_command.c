karo-electronics/karo-tx-uboot

commit 2513a2980c0e159ddb22b66c8711f8aff963130a

fastboot: add oem ucmd that supports sending u-boot commands via fastboot

drivers/fastboot/fb_command.c
@@ -41,6 +41,8 @@ static void reboot_recovery(char *, char *);
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_FORMAT)
 static void oem_format(char *, char *);
 #endif
+#if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_UCMD)
+static void oem_ucmd(char *, char *);
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_PARTCONF)
 static void oem_partconf(char *, char *);
 #endif
@@ -108,6 +110,12 @@ static const struct {
 		.command = "oem format",
 		.dispatch = oem_format,
 	},
+#endif
+#if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_UCMD)
+	[FASTBOOT_COMMAND_OEM_UCMD] = {
+                .command = "oem ucmd",
+                .dispatch = oem_ucmd
+        },
 #endif
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_PARTCONF)
 	[FASTBOOT_COMMAND_OEM_PARTCONF] = {
@@ -456,6 +464,31 @@ static void oem_format(char *cmd_parameter, char *response)
 			fastboot_fail("", response);
 		else
 			fastboot_okay(NULL, response);
+	}
+}
+#endif
+
+#if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_UCMD)
+/**
+ * ucmd() - Execute the ucmd command
+ *
+ * @cmd_parameter: Pointer to command parameter
+ * @response: Pointer to fastboot response buffer
+ */
+static void oem_ucmd(char *cmd_parameter, char *response)
+{
+	if (!cmd_parameter) {
+		pr_err("missing slot suffix\n");
+		fastboot_fail("missing command", response);
+		return;
+	}
+	if(run_command(cmd_parameter, 0)) {
+		fastboot_fail("", response);
+	} else {
+		fastboot_okay(NULL, response);
+		/* cmd may impact fastboot related environment*/
+                // TODO
+		// fastboot_setup();
 	}
 }
 #endif

drivers/fastboot/Kconfig
@@ -204,6 +204,13 @@ config FASTBOOT_CMD_OEM_FORMAT
 	  relies on the env variable partitions to contain the list of
 	  partitions as required by the gpt command.
 
+config FASTBOOT_CMD_OEM_UCMD
+	bool "Enable the 'oem ucmd' command"
+	depends on FASTBOOT
+	help
+	  Add support for the "oem ucmd" command from a client. This
+	  will allow you to send over u-boot commands from fastboot util.
+
 config FASTBOOT_CMD_OEM_PARTCONF
 	bool "Enable the 'oem partconf' command"
 	depends on FASTBOOT_FLASH_MMC && SUPPORT_EMMC_BOOT

include/fastboot.h
@@ -38,6 +38,9 @@ enum {
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_FORMAT)
 	FASTBOOT_COMMAND_OEM_FORMAT,
 #endif
+#if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_UCMD)
+	FASTBOOT_COMMAND_OEM_UCMD,
+#endif
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_PARTCONF)
 	FASTBOOT_COMMAND_OEM_PARTCONF,
 #endif

commit 92801a600ee642395ef57eac578a61348a93a414

fastboot: add missing #endif
Fixes: 2513a2980c0e ("fastboot: add oem ucmd that supports sending u-boot commands via fastboot")

drivers/fastboot/fb_command.c
@@ -43,6 +43,7 @@ static void oem_format(char *, char *);
 #endif
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_UCMD)
 static void oem_ucmd(char *, char *);
+#endif
 #if CONFIG_IS_ENABLED(FASTBOOT_CMD_OEM_PARTCONF)
 static void oem_partconf(char *, char *);
 #endif