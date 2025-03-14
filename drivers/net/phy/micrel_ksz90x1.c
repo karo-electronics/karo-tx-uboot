karo-electronics/karo-tx-uboot

commit 2ede4752e81f2a7ee55d4c202f30ff071be4c86e

net: phy: micrel: fix skew configuration

drivers/net/phy/micrel_ksz90x1.c
@@ -212,6 +212,94 @@ static int ksz9031_center_flp_timing(struct phy_device *phydev)
 	return ret;
 }
 
+static int ksz9131_of_config_group(struct phy_device *phydev,
+				   struct ksz90x1_ofcfg *ofcfg)
+{
+	struct udevice *dev = phydev->dev;
+	struct phy_driver *drv = phydev->drv;
+	int i, changed = 0, max;
+	u16 regval = 0;
+	ofnode node;
+	struct ofnode_phandle_args phandle_args;
+	int ret;
+
+	if (!drv || !drv->writeext)
+		return -EOPNOTSUPP;
+
+	ret = dev_read_phandle_with_args(dev, "phy-handle", NULL, 0, 0,
+					 &phandle_args);
+	if (!ret) {
+		node = phandle_args.node;
+	} else {
+		/* Look for a PHY node under the Ethernet node */
+		node = dev_read_subnode(dev, "ethernet-phy");
+		if (!ofnode_valid(node))
+			/* No node found, look in the Ethernet node */
+			node = dev_ofnode(dev);
+	}
+
+	for (i = 0; i < ofcfg->grpsz; i++) {
+		struct ksz90x1_reg_field *grp = &ofcfg->grp[i];
+		int step = grp->size == 5 ? 41 : 50;
+		int offset = grp->size == 5 ? 290 : 355;
+		int val;
+
+		if (ofnode_read_s32(node, grp->name, &val)) {
+			regval |= grp->dflt << grp->off;
+		} else {
+			val = (val + offset) / step;
+			changed = 1;	/* Value was changed in OF */
+			/* Calculate the register value and fix corner cases */
+			max = (1 << grp->size) - 1;
+			if (val > max)
+				regval |= max << grp->off;
+			else if (val > 0)
+				regval |= val << grp->off;
+		}
+	}
+
+	if (!changed)
+		return 0;
+
+	return drv->writeext(phydev, 0, ofcfg->devad, ofcfg->reg, regval);
+}
+
+static const struct ksz90x1_reg_field ksz9131_rxd_grp[] = {
+	{ "rxd0-skew-psec", 4, 0, 0x7 }, { "rxd1-skew-psec", 4, 4, 0x7 },
+	{ "rxd2-skew-psec", 4, 8, 0x7 }, { "rxd3-skew-psec", 4, 12, 0x7 },
+};
+
+static const struct ksz90x1_reg_field ksz9131_txd_grp[] = {
+	{ "txd0-skew-psec", 4, 0, 0x7 }, { "txd1-skew-psec", 4, 4, 0x7 },
+	{ "txd2-skew-psec", 4, 8, 0x7 }, { "txd3-skew-psec", 4, 12, 0x7 },
+};
+
+static const struct ksz90x1_reg_field ksz9131_ctl_grp[] = {
+	{ "txen-skew-psec", 4, 0, 0x7 }, { "rxdv-skew-psec", 4, 4, 0x7 },
+};
+
+static const struct ksz90x1_reg_field ksz9131_clk_grp[] = {
+	{ "rxc-skew-psec", 5, 0, 0x7 }, { "txc-skew-psec", 5, 5, 0x7 },
+};
+
+static int ksz9131_of_config(struct phy_device *phydev)
+{
+	struct ksz90x1_ofcfg ofcfg[] = {
+		{ MII_KSZ9031_EXT_RGMII_CTRL_SIG_SKEW, 2, ksz9131_ctl_grp, 2 },
+		{ MII_KSZ9031_EXT_RGMII_RX_DATA_SKEW, 2, ksz9131_rxd_grp, 4 },
+		{ MII_KSZ9031_EXT_RGMII_TX_DATA_SKEW, 2, ksz9131_txd_grp, 4 },
+		{ MII_KSZ9031_EXT_RGMII_CLOCK_SKEW, 2, ksz9131_clk_grp, 2 },
+	};
+	int i, ret = 0;
+
+	for (i = 0; i < ARRAY_SIZE(ofcfg); i++) {
+		ret = ksz9131_of_config_group(phydev, &ofcfg[i]);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
 #else /* !CONFIG_DM_ETH */
 static int ksz9021_of_config(struct phy_device *phydev)
 {
@@ -219,6 +307,11 @@ static int ksz9021_of_config(struct phy_device *phydev)
 }
 
 static int ksz9031_of_config(struct phy_device *phydev)
+{
+	return 0;
+}
+
+static int ksz9131_of_config(struct phy_device *phydev)
 {
 	return 0;
 }
@@ -490,6 +583,10 @@ static int ksz9131_config(struct phy_device *phydev)
 
 	if (phy_interface_is_rgmii(phydev)) {
 		ret = ksz9131_config_rgmii_delay(phydev);
+		if (ret)
+			return ret;
+
+		ret = ksz9131_of_config(phydev);
 		if (ret)
 			return ret;
 	}

commit 55b528941ef118e7f0ec7ee6340cae7fb415bdf8

net: phy: micrel: fix 'initialization discards 'const' qualifier from pointer target type' bug introduced by commit 2ede4752e81f

drivers/net/phy/micrel_ksz90x1.c
@@ -239,7 +239,7 @@ static int ksz9131_of_config_group(struct phy_device *phydev,
 	}
 
 	for (i = 0; i < ofcfg->grpsz; i++) {
-		struct ksz90x1_reg_field *grp = &ofcfg->grp[i];
+		const struct ksz90x1_reg_field *grp = &ofcfg->grp[i];
 		int step = grp->size == 5 ? 41 : 50;
 		int offset = grp->size == 5 ? 290 : 355;
 		int val;