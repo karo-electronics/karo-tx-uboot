u-boot/u-boot

commit 2003a83cc8b3937cda09e712c75f50f579eed3fa

spl: Avoid an #ifdef when printing gd->malloc_ptr
Use an accessor in the header file to avoid this.

Signed-off-by: Simon Glass <sjg@chromium.org>

include/asm-generic/global_data.h
@@ -588,6 +588,12 @@ static_assert(sizeof(struct global_data) == GD_SIZE);
 #define gd_set_pci_ram_top(val)
 #endif
 
+#if CONFIG_VAL(SYS_MALLOC_F_LEN)
+#define gd_malloc_ptr()		gd->malloc_ptr
+#else
+#define gd_malloc_ptr()		0L
+#endif
+
 /**
  * enum gd_flags - global data flags
  *

common/spl/spl.c
@@ -860,10 +860,11 @@ void board_init_r(gd_t *dummy1, ulong dummy2)
 	} else {
 		debug("Unsupported OS image.. Jumping nevertheless..\n");
 	}
-#if CONFIG_IS_ENABLED(SYS_MALLOC_F) && !defined(CONFIG_SPL_SYS_MALLOC_SIZE)
-	debug("SPL malloc() used 0x%lx bytes (%ld KB)\n", gd->malloc_ptr,
-	      gd->malloc_ptr / 1024);
-#endif
+	if (CONFIG_IS_ENABLED(SYS_MALLOC_F) &&
+	    !IS_ENABLED(CONFIG_SPL_SYS_MALLOC_SIZE))
+		debug("SPL malloc() used 0x%lx bytes (%ld KB)\n",
+		      gd_malloc_ptr(), gd_malloc_ptr() / 1024);
+
 	bootstage_mark_name(get_bootstage_id(false), "end phase");
 #ifdef CONFIG_BOOTSTAGE_STASH
 	ret = bootstage_stash((void *)CONFIG_BOOTSTAGE_STASH_ADDR,

karo-electronics/karo-tx-uboot

commit b7d184bedd1e34751e8465cdfba81771ac195ba6

Fix type of 'gd_malloc_ptr()' macro definition
The macro gd_malloc_ptr() may either return the value of 'gd->malloc_ptr'
(which is of type unsigned int) or the constant '0L' (type long) depending
on the preprocessor symbol CONFIG_SYS_MALLOC_F_LEN.

Change the constant definition to ensure that the macro returns the
same type in any case to satisfy the format string in the debug
message (the only user of the gd_malloc_ptr() macro) in
common/spl/spl.c:
|		debug("SPL malloc() used 0x%x bytes (%d KB)\n",
|		      gd_malloc_ptr(), gd_malloc_ptr() / 1024);

Fixes: commit 2003a83cc8b3 ("spl: Avoid an #ifdef when printing gd->malloc_ptr")

include/asm-generic/global_data.h
@@ -538,7 +538,7 @@ static_assert(sizeof(struct global_data) == GD_SIZE);
 #if CONFIG_VAL(SYS_MALLOC_F_LEN)
 #define gd_malloc_ptr()		gd->malloc_ptr
 #else
-#define gd_malloc_ptr()		0L
+#define gd_malloc_ptr()		0
 #endif
 
 #if CONFIG_IS_ENABLED(UPL)