u-boot/u-boot

commit 1859b8b60d177545a284e67d9f08011810b37312

imx: move get_boot_device to common header
Most i.MX implements get_boot_device, move it to common header to
simplify code

Signed-off-by: Peng Fan <peng.fan@nxp.com>

arch/arm/include/asm/mach-imx/sys_proto.h
@@ -246,4 +246,6 @@ void imx_get_mac_from_fuse(int dev_id, unsigned char *mac);
 void enable_ca7_smp(void);
 #endif
 
+enum boot_device get_boot_device(void);
+
 #endif

arch/arm/include/asm/arch-imx8/sys_proto.h
@@ -23,7 +23,6 @@ struct pass_over_info_t {
 
 extern unsigned long boot_pointer[];
 void build_info(void);
-enum boot_device get_boot_device(void);
 int print_bootinfo(void);
 int sc_pm_setup_uart(sc_rsrc_t uart_rsrc, sc_pm_clock_rate_t clk_rate);
 int imx8_power_domain_lookup_name(const char *name,

arch/arm/include/asm/arch-imx8m/sys_proto.h
@@ -12,6 +12,5 @@ void set_wdog_reset(struct wdog_regs *wdog);
 void enable_tzc380(void);
 void restore_boot_params(void);
 extern unsigned long rom_pointer[];
-enum boot_device get_boot_device(void);
 bool is_usb_boot(void);
 #endif

arch/arm/include/asm/arch-imx8ulp/sys_proto.h
@@ -15,7 +15,6 @@ ulong spl_romapi_get_uboot_base(u32 image_offset, u32 rom_bt_dev);
 enum bt_mode get_boot_mode(void);
 int xrdc_config_pdac(u32 bridge, u32 index, u32 dom, u32 perm);
 int xrdc_config_pdac_openacc(u32 bridge, u32 index);
-enum boot_device get_boot_device(void);
 void set_lpav_qos(void);
 void load_lposc_fuse(void);
 bool m33_image_booted(void);

arch/arm/include/asm/arch-mx7/sys_proto.h
@@ -8,6 +8,5 @@
 #include <asm/mach-imx/sys_proto.h>
 
 void set_wdog_reset(struct wdog_regs *wdog);
-enum boot_device get_boot_device(void);
 
 #endif /* __SYS_PROTO_IMX7_ */

arch/arm/include/asm/arch-mx7ulp/sys_proto.h
@@ -8,5 +8,4 @@
 
 #include <asm/mach-imx/sys_proto.h>
 
-enum boot_device get_boot_device(void);
 #endif

karo-electronics/karo-tx-uboot

commit bda8540112ad4ac5e8873dce286b907dcf9e0321

imx: make arch/arm/include/asm/mach-imx/sys_proto.h self-contained
The declaration 'enum boot_device get_boot_device(void);' requires the
'enum boot_device' to be declared.

Add missing include to not require including
asm/arch/mach-imx/boot_mode.h before asm/arch/sys_proto.h and follow
the ordering scheme described in:
https://docs.u-boot.org/en/latest/develop/codingstyle.html#include-files.

Fixes: 1859b8b60d17 ("imx: move get_boot_device to common header")

arch/arm/include/asm/mach-imx/sys_proto.h
@@ -11,6 +11,7 @@
 #include <asm/mach-imx/regs-common.h>
 #include <asm/mach-imx/module_fuse.h>
 #include <linux/bitops.h>
+#include "boot_mode.h"
 #include "../arch-imx/cpu.h"
 
 struct bd_info;