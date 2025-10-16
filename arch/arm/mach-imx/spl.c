u-boot/u-boot

commit 511db3bf5a27a2f7eca0f60b6adb1eff74bd6df6

toradex: imx6: Move g_dnl_bind_fixup() into common SPL code
Instead of having every board file to add its own g_dnl_bind_fixup()
implementation, move it to the common imx6 SPL code.

Signed-off-by: Fabio Estevam <fabio.estevam@nxp.com>
Reviewed-by: Łukasz Majewski <lukma@denx.de>

arch/arm/mach-imx/spl.c
@@ -14,6 +14,7 @@
 #include <asm/spl.h>
 #include <spl.h>
 #include <asm/mach-imx/hab.h>
+#include <g_dnl.h>
 
 DECLARE_GLOBAL_DATA_PTR;
 
@@ -94,6 +95,15 @@ u32 spl_boot_device(void)
 	}
 	return BOOT_DEVICE_NONE;
 }
+
+#ifdef CONFIG_SPL_USB_GADGET_SUPPORT
+int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
+{
+	put_unaligned(CONFIG_G_DNL_PRODUCT_NUM + 0xfff, &dev->idProduct);
+
+	return 0;
+}
+#endif
 #endif
 
 #if defined(CONFIG_SPL_MMC_SUPPORT)

board/toradex/apalis_imx6/apalis_imx6.c
@@ -29,7 +29,6 @@
 #include <dm/platform_data/serial_mxc.h>
 #include <dm/platdata.h>
 #include <fsl_esdhc.h>
-#include <g_dnl.h>
 #include <i2c.h>
 #include <imx_thermal.h>
 #include <linux/errno.h>
@@ -1223,18 +1222,6 @@ void board_init_f(ulong dummy)
 void reset_cpu(ulong addr)
 {
 }
-
-#ifdef CONFIG_SPL_USB_GADGET_SUPPORT
-int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
-{
-	unsigned short usb_pid;
-
-	usb_pid = TORADEX_USB_PRODUCT_NUM_OFFSET + 0xfff;
-	put_unaligned(usb_pid, &dev->idProduct);
-
-	return 0;
-}
-#endif
 
 #endif
 

board/toradex/colibri_imx6/colibri_imx6.c
@@ -28,7 +28,6 @@
 #include <dm/platform_data/serial_mxc.h>
 #include <dm/platdata.h>
 #include <fsl_esdhc.h>
-#include <g_dnl.h>
 #include <i2c.h>
 #include <imx_thermal.h>
 #include <linux/errno.h>
@@ -1107,18 +1106,6 @@ void board_init_f(ulong dummy)
 void reset_cpu(ulong addr)
 {
 }
-
-#ifdef CONFIG_SPL_USB_GADGET_SUPPORT
-int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
-{
-	unsigned short usb_pid;
-
-	usb_pid = TORADEX_USB_PRODUCT_NUM_OFFSET + 0xfff;
-	put_unaligned(usb_pid, &dev->idProduct);
-
-	return 0;
-}
-#endif
 
 #endif
 

karo-electronics/karo-tx-uboot

commit a294dbd12a21862d76a150431b7d066406a970fc

arm: imx: Fix bogus setting of USB Product ID
The patch 511db3bf5a27 ("toradex: imx6: Move g_dnl_bind_fixup() into common SPL code")
introduced a Toradex specific setting of the USB gadget product id
into common code breaking the USB boot for i.MX8MM platforms.

The UUU loader expects the product id 0x0151 in the second stage of
the boot process for i.MX8MM processors.

Fixes: 511db3bf5a27 ("toradex: imx6: Move g_dnl_bind_fixup() into common SPL code")

arch/arm/mach-imx/spl.c
@@ -184,7 +184,7 @@ u32 spl_boot_device(void)
 #ifdef CONFIG_SPL_USB_GADGET
 int g_dnl_bind_fixup(struct usb_device_descriptor *dev, const char *name)
 {
-	put_unaligned(CONFIG_USB_GADGET_PRODUCT_NUM + 0xfff, &dev->idProduct);
+	put_unaligned(0x151, &dev->idProduct);
 
 	return 0;
 }